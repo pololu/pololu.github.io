6d3fbdca470c7c472478ac4fced5b72951a958b5
David (Pololu) <dev-david@pololu.com>
2014-12-31 16:38:03 -0800

    Updated the a-star-32u4-prime documentation to version 1.1.0.

--- a-star-32u4-prime/search/functions_a.js
+++ a-star-32u4-prime/search/functions_a.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['readbatterymillivolts',['readBatteryMillivolts',['../_a_star32_u4_prime_8h.html#a043505afa78c02cc8caae0521c8cfa8a',1,'AStar32U4Prime.h']]],
+  ['readbatterymillivoltslv',['readBatteryMillivoltsLV',['../_a_star32_u4_prime_8h.html#acb84fb4e1a6980db0a7eea6487958593',1,'AStar32U4Prime.h']]],
+  ['readbatterymillivoltssv',['readBatteryMillivoltsSV',['../_a_star32_u4_prime_8h.html#ab4006e57c2c8475f68ad4a5ea7dbdc2a',1,'AStar32U4Prime.h']]],
   ['reinitialize',['reinitialize',['../class_pololu_h_d44780_base.html#a10c1c42406708172fc38b718790ba881',1,'PololuHD44780Base']]],
   ['righttoleft',['rightToLeft',['../class_pololu_h_d44780_base.html#aa3f8d4ba18feb9aa0f0a2fef3c6c2b37',1,'PololuHD44780Base']]]
 ];
