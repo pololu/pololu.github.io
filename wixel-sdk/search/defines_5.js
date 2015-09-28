32ed21123c1c5c7b500f7759154afd3cee97f920
David (Pololu) <dev-david@pololu.com>
2015-09-28 11:55:22 -0700

    wixel-sdk: Updated to faaf3e30c8.

--- wixel-sdk/search/defines_5.js
+++ wixel-sdk/search/defines_5.js
@@ -1,12 +1,12 @@
 var searchData=
 [
-  ['led_5fgreen',['LED_GREEN',['../board_8h.html#a4c2ae8ec082dea793648ffcfa5012576',1,'board.h']]],
+  ['led_5fgreen',['LED_GREEN',['../board_8h.html#a631980ac0aba7e723b26dd869f4f82c9',1,'board.h']]],
   ['led_5fgreen_5fstate',['LED_GREEN_STATE',['../board_8h.html#a09e713cb9c9c704feaee943f31a81650',1,'board.h']]],
   ['led_5fgreen_5ftoggle',['LED_GREEN_TOGGLE',['../board_8h.html#a4340dbdac247a5874e7f2bf9c45033b3',1,'board.h']]],
-  ['led_5fred',['LED_RED',['../board_8h.html#a2fc7d0b0ed4c03027fcadb16237395b0',1,'board.h']]],
+  ['led_5fred',['LED_RED',['../board_8h.html#a4717312cd7fb8ff519a6b5d368780e04',1,'board.h']]],
   ['led_5fred_5fstate',['LED_RED_STATE',['../board_8h.html#a12c337ce7495ff82914e5acd8a462e15',1,'board.h']]],
-  ['led_5fred_5ftoggle',['LED_RED_TOGGLE',['../board_8h.html#a00bd45e31c7a540b6395bae41f89d44f',1,'board.h']]],
-  ['led_5fyellow',['LED_YELLOW',['../board_8h.html#ae9fca47460487639382316132d18564e',1,'board.h']]],
+  ['led_5fred_5ftoggle',['LED_RED_TOGGLE',['../board_8h.html#a73b0d98e166afb75dc7609e12716dc4c',1,'board.h']]],
+  ['led_5fyellow',['LED_YELLOW',['../board_8h.html#a26149bce47682ec3fdfdc4b5c64d5835',1,'board.h']]],
   ['led_5fyellow_5fstate',['LED_YELLOW_STATE',['../board_8h.html#ac77f725a3aa6c1aa945f17b4f180c669',1,'board.h']]],
   ['led_5fyellow_5ftoggle',['LED_YELLOW_TOGGLE',['../board_8h.html#ab861a3d66f882892f825e03cf121b5c9',1,'board.h']]],
   ['low',['LOW',['../gpio_8h.html#ab811d8c6ff3a505312d3276590444289',1,'gpio.h']]]
---
+++ wixel-sdk/search/searchdata.js
@@ -0,0 +1,42 @@
+var indexSectionsWithContent =
+{
+  0: "_abcdeghiklmprstuvwxyz",
+  1: "acdhsu",
+  2: "abcdgirstu",
+  3: "abdegirstuv",
+  4: "_bdiklmprstuvwxyz",
+  5: "abdiu",
+  6: "u",
+  7: "acdhilprsux",
+  8: "w",
+  9: "w"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "enums",
+  7: "defines",
+  8: "groups",
+  9: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Typedefs",
+  6: "Enumerations",
+  7: "Macros",
+  8: "Modules",
+  9: "Pages"
+};
+
