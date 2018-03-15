7a1411f1fda65067ad346ab00f22662e0f218aed
RyanTM (Pololu) <dev-ryantm@pololu.com>
2018-03-15 15:15:55 -0700

    update zumo-shield-arduino-library documentation

---
+++ zumo-shield-arduino-library/search/all_a.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['zumobuzzer',['ZumoBuzzer',['../classZumoBuzzer.html',1,'']]],
+  ['zumobuzzer_2eh',['ZumoBuzzer.h',['../ZumoBuzzer_8h.html',1,'']]],
+  ['zumomotors',['ZumoMotors',['../classZumoMotors.html',1,'']]],
+  ['zumomotors_2eh',['ZumoMotors.h',['../ZumoMotors_8h.html',1,'']]],
+  ['zumoreflectancesensorarray',['ZumoReflectanceSensorArray',['../classZumoReflectanceSensorArray.html',1,'ZumoReflectanceSensorArray'],['../classZumoReflectanceSensorArray.html#a19f33305b41d2dcb6d637474571a9e3f',1,'ZumoReflectanceSensorArray::ZumoReflectanceSensorArray()'],['../classZumoReflectanceSensorArray.html#a0622d191a377bc6d379693f60e55c681',1,'ZumoReflectanceSensorArray::ZumoReflectanceSensorArray(unsigned char emitterPin)'],['../classZumoReflectanceSensorArray.html#abc268241282ff838faefd2cc0e66c1e8',1,'ZumoReflectanceSensorArray::ZumoReflectanceSensorArray(unsigned char *pins, unsigned char numSensors, unsigned int timeout=2000, unsigned char emitterPin=ZUMO_SENSOR_ARRAY_DEFAULT_EMITTER_PIN)']]],
+  ['zumoreflectancesensorarray_2eh',['ZumoReflectanceSensorArray.h',['../ZumoReflectanceSensorArray_8h.html',1,'']]]
+];
---
+++ zumo-shield-arduino-library/search/classes_3.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['vector',['vector',['../structLSM303_1_1vector.html',1,'LSM303']]],
+  ['vector',['vector',['../structL3G_1_1vector.html',1,'L3G']]],
+  ['vector_3c_20int16_5ft_20_3e',['vector&lt; int16_t &gt;',['../structL3G_1_1vector.html',1,'L3G::vector&lt; int16_t &gt;'],['../structLSM303_1_1vector.html',1,'LSM303::vector&lt; int16_t &gt;']]]
+];
