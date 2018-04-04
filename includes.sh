a136280f83388d9e30b9c4031185ebd8e3653d5d
David (Pololu) <dev-david@pololu.com>
2018-04-04 16:46:47 -0700

    Fix _config.yml to just have every HTML file name in it because apparently GitHub Pages doesn't like names like Tic_8h.html unless they are in listed in _config.yml.  I'm not sure what all the rules are so I want to just list every html file.

--- includes.sh
+++ includes.sh
@@ -1,6 +1,6 @@
-# Generates the Jekyll configuration file that is required
-# so that we can have underscores in our HTML file names.
+# Generates the Jekyll configuration file that is required so that we
+# can have underscores or capital letters in our HTML file names.
 
 echo -n 'include: ['
-find . -name '_*.html' | awk -F/ "{printf \"'%s', \", \$NF}"
+find . -name '*.html' | awk -F/ "{printf \"'%s', \", \$NF}"
 echo ]
