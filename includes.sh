59f8b0f0b783c11bcce182d2c4a6f2c1be8c1272
Kevin (Pololu) <dev-kevin@pololu.com>
2020-12-04 15:38:08 -0800

    add .nojekyll; remove _config.yml workaround for files starting with underscores

--- includes.sh
+++
@@ -1,6 +0,0 @@
-# Generates the Jekyll configuration file that is required so that we
-# can have underscores or capital letters in our HTML file names.
-
-echo -n 'include: ['
-find . -name '*.html' | awk -F/ "{printf \"'%s', \", \$NF}"
-echo ]
